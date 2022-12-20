f0bb1702f7b341c82f5071a28ba8a00315211a29
Sililo <sililosiaosi3000@gmail.com>
2022-12-20 23:00:04 +1300

    Added functionality, more quotes required

--- QuoteGenerator.js
+++ QuoteGenerator.js
@@ -3,3 +3,14 @@ const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
 const hook = ["look", "remember", "never forget", "dont worry", "chin up", "stay strong"]
 
 const quote = ["it is what it is"]
+
+let currentQuote = ""
+
+const generate_quote = (day) => {
+    let chosenHook = hook[Math.floor(Math.random() * hook.length)];
+    let chosenQuote = quote[Math.floor(Math.random() * quote.length)];
+    currentQuote = `${day}'s quote: ${chosenHook}, ${chosenQuote}`;
+    return currentQuote;
+}
+
+console.log(generate_quote("Monday"))
