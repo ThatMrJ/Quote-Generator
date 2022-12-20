0fd7934e8a8467e5111bfde9446b64e3f1c976dd
Sililo <sililosiaosi3000@gmail.com>
2022-12-20 22:48:30 +1300

    data test

---
+++ QuoteGenerator.js
@@ -0,0 +1,5 @@
+const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
+
+const hook = ["look", "remember", "never forget", "dont worry", "chin up", "stay strong"]
+
+const quote = ["it is what it is"]
